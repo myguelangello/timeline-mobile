00af4edcf6efce9c9cf32d61adb2cf18458e3584
myguelangello <myguelangello624@gmail.com>
2023-05-20 21:13:54 -0300

    Setup Base Mobile

--- App.tsx
+++ App.tsx
@@ -1,21 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { Text, View } from 'react-native'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <View className="flex-1 items-center justify-center bg-zinc-900">
+      <StatusBar style="light" translucent />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      <Text className="text-5xl font-bold text-gray-50">Hello World!</Text>
+    </View>
+  )
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# App de linha do tempo
+
+  ### Setup do Projeto Mobile
+
+  #### Features
+  
+    ✅ React Native
+    ✅ TypeScript
+    ✅ Expo GO
+    ✅ NativeWind (TailwindCSS para React Native)
+    ✅ ESLint + Prettier Tailwind
